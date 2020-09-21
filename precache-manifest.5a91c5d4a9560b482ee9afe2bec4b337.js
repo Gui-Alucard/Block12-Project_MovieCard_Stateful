22869a35c88340075d9e2bf0b1634138d0b8094e
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-09-21 19:54:31 -0300

    Updates

---
+++ precache-manifest.5a91c5d4a9560b482ee9afe2bec4b337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d1fe54fc5c15aa05513264b4af9e1e",
+    "url": "/Block12-Project_MovieCard_Stateful/index.html"
+  },
+  {
+    "revision": "fec6712a2870b49b6ea6",
+    "url": "/Block12-Project_MovieCard_Stateful/static/css/main.907bc088.chunk.css"
+  },
+  {
+    "revision": "cdcdad0f1f4b1ac37bf3",
+    "url": "/Block12-Project_MovieCard_Stateful/static/js/2.416c5706.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Block12-Project_MovieCard_Stateful/static/js/2.416c5706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec6712a2870b49b6ea6",
+    "url": "/Block12-Project_MovieCard_Stateful/static/js/main.2004725c.chunk.js"
+  },
+  {
+    "revision": "d5c1139f4f368d812265",
+    "url": "/Block12-Project_MovieCard_Stateful/static/js/runtime-main.02a49bd5.js"
+  }
+]);
